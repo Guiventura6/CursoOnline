bc481b58854a38c6722e7710d6cc0fe4aeac1341
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-01-07 10:01:00 -0300

    Criacao da classe Student.

---
+++ CursoOnline/Entities/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoOnline.Entities
+{
+    class Student
+    {
+        public int Cod { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Cod.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Student))
+            {
+                return false;
+            }
+            Student student = obj as Student;
+            return Cod.Equals(student.Cod);
+        }
+    }
+}
