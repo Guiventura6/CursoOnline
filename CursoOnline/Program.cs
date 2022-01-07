935eac769a5b454657b9c2c63724ff688c6a0eb4
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-01-07 10:15:29 -0300

    Implementacao da entrada e dados e impressao do total de alunos no arquivo program.

--- CursoOnline/Program.cs
+++ CursoOnline/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using CursoOnline.Entities;
+using System.Collections.Generic;
 
 namespace CursoOnline
 {
@@ -6,7 +8,33 @@ namespace CursoOnline
     {
         static void Main(string[] args)
         {
-            
+            HashSet<Student> students = new HashSet<Student>();
+
+            Console.Write("How many students for course A? ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int cod = int.Parse(Console.ReadLine());
+                students.Add(new Student { Cod = cod });
+            }
+
+            Console.Write("How many students for course B? ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int cod = int.Parse(Console.ReadLine());
+                students.Add(new Student { Cod = cod });
+            }
+
+            Console.Write("How many students for course C? ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int cod = int.Parse(Console.ReadLine());
+                students.Add(new Student { Cod = cod });
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Total students: {students.Count}");
         }
     }
 }
